1919cafe61600e290eb4742c26cfececca81f6f7
Evan You <yyx990803@gmail.com>
2015-10-26 21:00:49 -0400

    send to console

--- src/panel/Inspector.vue
+++ src/panel/Inspector.vue
@@ -2,6 +2,7 @@
   <div>
     <p>{{ target.name }}</p>
     <a @click="inspectDOM">Inspect DOM</a>
+    <a @click="sendToConsole">Send to Console</a>
     <pre>{{ target.state | json }}</pre>
     <pre>{{ target.props | json }}</pre>
     <pre>{{ target.computed | json }}</pre>
@@ -17,6 +18,11 @@ export default {
     inspectDOM () {
       chrome.devtools.inspectedWindow.eval(
         `inspect(window.__VUE_DEVTOOLS_INSTANCE_MAP__.get(${ this.target.id }).$el)`
+      )
+    },
+    sendToConsole () {
+      chrome.devtools.inspectedWindow.eval(
+        `$vm = window.__VUE_DEVTOOLS_INSTANCE_MAP__.get(${ this.target.id })`
       )
     }
   }
