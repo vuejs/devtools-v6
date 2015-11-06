2667bdc433247e4933ae5cd9ea3ce86d1d69ca57
Evan You <yyx990803@gmail.com>
2015-11-06 15:25:08 -0500

    walk documentElement instead of body

--- src/backend/index.js
+++ src/backend/index.js
@@ -79,7 +79,7 @@ function scan () {
   rootInstances.length = 0
   let inFragment = false
   let currentFragment = null
-  walk(document.body, function (node) {
+  walk(document.documentElement, function (node) {
     if (inFragment) {
       if (node === currentFragment._fragmentEnd) {
         inFragment = false
