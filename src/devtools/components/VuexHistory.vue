b69ee9c0e0377cdd3e6a859829beae9e5ef89f34
Bartek Iwańczuk <biwanczuk@gmail.com>
2016-11-12 16:04:21 +0100

    adds mutation filtering to Vuex tab (#172)

--- src/devtools/components/VuexHistory.vue
+++ src/devtools/components/VuexHistory.vue
@@ -13,6 +13,14 @@
         <i class="material-icons">cached</i>
         <span>Reset</span>
       </a>
+      <div class="search">
+        <i class="material-icons">search</i>
+        <input class="search-box" placeholder="Filter mutations" v-model="userInputFilter">
+        <span class="invalid-regex" v-show="invalidRegex">Invalid RegEx!</span>
+      </div>
+      <a class="button" @click="clearFilter" v-show="userInputFilter">
+        <i class="material-icons">clear</i>
+      </a>
     </div>
     <div class="history">
       <div class="entry"
@@ -24,7 +32,7 @@
         </span>
       </div>
       <div class="entry"
-        v-for="(entry, index) in history"
+        v-for="(entry, index) in filteredHistory"
         :class="{ active: activeIndex === index }"
         @click="step(index)">
         <span class="mutation-type">{{ entry.mutation.type }}</span>
@@ -50,13 +58,44 @@
 import keyNavMixin from '../mixins/key-nav'
 import { mapState, mapActions } from 'vuex'
 
+const REGEX_RE = /^\/(.*?)\/(\w*)/
+
 export default {
   mixins: [keyNavMixin],
-  computed: mapState({
-    history: state => state.vuex.history,
-    lastCommit: state => state.vuex.lastCommit,
-    activeIndex: state => state.vuex.activeIndex
-  }),
+  data () {
+    return {
+      userInputFilter: '',
+      invalidRegex: false
+    }
+  },
+  computed: {
+    ...mapState({
+      history: state => state.vuex.history,
+      lastCommit: state => state.vuex.lastCommit,
+      activeIndex: state => state.vuex.activeIndex
+    }),
+    compiledFilter () {
+      const regexParts = this.userInputFilter.match(REGEX_RE)
+      if (regexParts !== null) {
+        // looks like it might be a regex -> try to compile it
+        try {
+          const re = new RegExp(regexParts[1], regexParts[2])
+          this.invalidRegex = false
+          return re
+        } catch (e) {
+          this.invalidRegex = true
+          return new RegExp('.*', 'i')
+        }
+      }
+      // simple case-insensitve search
+      return new RegExp(this.escapeStringForRegExp(this.userInputFilter), 'i')
+    },
+    filteredHistory () {
+      return this.history.filter((entry) => {
+        return this.compiledFilter.test(entry.mutation.type)
+      })
+    }
+  },
   filters: {
     formatTime (timestamp) {
       return (new Date(timestamp)).toString().match(/\d\d:\d\d:\d\d/)[0]
@@ -77,6 +116,13 @@ export default {
       } else if (dir === 'down') {
         this.step(this.activeIndex + 1)
       }
+    },
+    clearFilter () {
+      this.userInputFilter = ''
+      this.invalidRegex = false
+    },
+    escapeStringForRegExp (str) {
+      return str.replace(/[\-\[\]\/\{\}\(\)\*\+\?\.\\\^\$\|]/g, '\\$&')
     }
   }
 }
@@ -88,7 +134,11 @@ $blue = #44A1FF
 .buttons
   padding 15px 30px 5px 20px
   border-bottom 1px solid #eee
-
+  .material-icons
+    font-size 16px
+  .material-icons, span
+    vertical-align middle
+    
 .button
   color #555
   cursor pointer
@@ -101,11 +151,32 @@ $blue = #44A1FF
   &.disabled
     color #aaa
     cursor not-allowed
-  .material-icons
-    font-size 16px
-  .material-icons, span
-    vertical-align middle
 
+.search
+  width 300px
+  display inline-block
+  font-size 13px
+  margin 0 0 10px 0
+  box-sizing border-box
+
+.search-box
+  font-family Roboto
+  box-sizing border-box
+  color #666
+  position relative
+  z-index 0
+  vertical-align middle
+  font-size 13px
+  border none
+  outline none
+  width calc(100% - 110px)
+  background transparent
+
+.invalid-regex 
+  display inline-block
+  width 85px
+  color #ff4141
+  
 .history
   height calc(100% - 48px)
   overflow-x hidden
