32c464eccd20ff6b4d4cb3015fdde9869830d332
Evan You <yyx990803@gmail.com>
2017-01-04 16:47:37 -0500

    ignore whitespace

--- shells/chrome/webpack.config.js
+++ shells/chrome/webpack.config.js
@@ -38,6 +38,7 @@ module.exports = {
         test: /\.vue$/,
         loader: 'vue-loader',
         options: {
+          preserveWhitespace: false,
           buble: bubleOptions
         }
       },
