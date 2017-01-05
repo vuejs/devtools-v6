ee840a4a4130c8a630d2d0a4d1e59b071d5de4ef
Evan You <yyx990803@gmail.com>
2017-01-05 16:05:31 -0500

    try...catch localStorage access (fix #218)

--- src/devtools/storage.js
+++ src/devtools/storage.js
@@ -1,14 +1,25 @@
+// If the users blocks 3rd party cookies and storage,
+// localStorage will throw.
+
 export default {
   get (key) {
-    return JSON.parse(localStorage.getItem(key))
+    try {
+      return JSON.parse(localStorage.getItem(key))
+    } catch (e) {}
   },
   set (key, val) {
-    localStorage.setItem(key, JSON.stringify(val))
+    try {
+      localStorage.setItem(key, JSON.stringify(val))
+    } catch (e) {}
   },
   remove (key) {
-    localStorage.removeItem(key)
+    try {
+      localStorage.removeItem(key)
+    } catch (e) {}
   },
   clear () {
-    localStorage.clear()
+    try {
+      localStorage.clear()
+    } catch (e) {}
   }
 }
