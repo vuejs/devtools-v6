01fcadb8f32e485b554bd824cfdf334c5ec488ce
Evan You <yyx990803@gmail.com>
2017-01-06 11:13:19 -0500

    fix $emit patch compatibility with 1.x

--- src/backend/events.js
+++ src/backend/events.js
@@ -9,18 +9,21 @@ export function initEventsBackend (Vue, bridge, getInstanceName) {
 
   const vueEmit = Vue.prototype.$emit
   Vue.prototype.$emit = function () {
-    vueEmit.apply(this, arguments)
+    const res = vueEmit.apply(this, arguments)
 
-    if (!recording) return
-    const eventName = arguments[0]
-    if (!eventName.startsWith('hook:')) {
-      bridge.send('event:emit', stringify({
-        instanceId: this._uid,
-        instanceName: getInstanceName(this),
-        eventName: eventName,
-        eventData: arguments[1],
-        timestamp: Date.now()
-      }))
+    if (recording) {
+      const eventName = arguments[0]
+      if (!eventName.startsWith('hook:')) {
+        bridge.send('event:emit', stringify({
+          instanceId: this._uid,
+          instanceName: getInstanceName(this),
+          eventName: eventName,
+          eventData: arguments[1],
+          timestamp: Date.now()
+        }))
+      }
     }
+
+    return res
   }
 }
