98af5c92f2bb00abbe23b79a23313cba28ce76b1
armano2 <sponsor15@gmail.com>
2017-05-07 02:41:23 +0200

    Fix posible issue when there is no routes when routing is initialized.

--- src/backend/router.js
+++ src/backend/router.js
@@ -8,8 +8,9 @@ export function initRouterBackend (Vue, bridge, rootInstances) {
   })
 
   for (let i = 0; i < rootInstances.length; i++) {
-    if (rootInstances[i]._router) {
-      rootInstances[i]._router.afterEach((to, from) => {
+    const router = rootInstances[i]._router
+    if (router) {
+      router.afterEach((to, from) => {
         if (recording) {
           bridge.send('router:changed', stringify({
             to,
@@ -19,25 +20,28 @@ export function initRouterBackend (Vue, bridge, rootInstances) {
         }
       })
       bridge.send('router:init', stringify({
-        mode: rootInstances[i]._router.mode
+        mode: router.mode
       }))
 
       bridge.send('routes:init', stringify({
-        routes: rootInstances[i]._router.options.routes,
-        mode: rootInstances[i]._router.mode
+        routes: router.options.routes,
+        mode: router.mode
       }))
 
-      rootInstances[i]._router.options.routes.forEach(instance => {
-        bridge.send('routes:changed', stringify(instance))
-        console.log(instance)
-      })
-
-      const addRoutes = rootInstances[i]._router.matcher.addRoutes
-      rootInstances[i]._router.matcher.addRoutes = function (routes) {
-        routes.forEach((item) => {
-          bridge.send('routes:changed', stringify(item))
+      if (router.options && router.options.routes) {
+        router.options.routes.forEach(instance => {
+          bridge.send('routes:changed', stringify(instance))
         })
-        addRoutes.call(this, routes)
+      }
+
+      if (router.matcher && router.matcher.addRoutes) {
+        const addRoutes = router.matcher.addRoutes
+        router.matcher.addRoutes = function (routes) {
+          routes.forEach((item) => {
+            bridge.send('routes:changed', stringify(item))
+          })
+          addRoutes.call(this, routes)
+        }
       }
     }
   }
