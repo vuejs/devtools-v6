15c172406df56c11cbcc70ad54b45f64cc8fcf36
Jared Hobbs <jared@pyhacker.com>
2017-06-16 10:11:09 -0700

    add comment

--- src/bridge.js
+++ src/bridge.js
@@ -3,6 +3,10 @@ import { EventEmitter } from 'events'
 export default class Bridge extends EventEmitter {
   constructor (wall) {
     super()
+    // Setting `this` to `self` here to fix an error in the Safari build:
+    // ReferenceError: Cannot access uninitialized variable.
+    // The error might be related to the webkit bug here:
+    // https://bugs.webkit.org/show_bug.cgi?id=171543
     const self = this
     self.setMaxListeners(Infinity)
     self.wall = wall
