a7e182aca2ffd14ae8b2bfdb005ae4ea9697ec08
Guillaume Chau <alphadelta.fg@gmail.com>
2017-12-28 20:23:43 +0100

    (Demo) Use id prop

--- shells/dev/target/Target.vue
+++ shells/dev/target/Target.vue
@@ -8,7 +8,7 @@
     <button class="add" @click="add">Add</button>
     <button class="remove" @click="rm">Remove</button>
     <input v-model="localMsg">
-    <other v-for="item in items" :key="item"></other>
+    <other v-for="item in items" :key="item" :id="item"></other>
   </div>
 </template>
 
