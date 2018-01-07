bd5fbf225ffd98f72f861402211cfb9c9dee68ec
Michał Sajnóg <msajnog93@gmail.com>
2018-01-07 23:21:34 +0100

    Use consistent port, update electrom shell template to display proper IP

--- shells/electron/bin.js
+++ shells/electron/bin.js
@@ -1,11 +1,11 @@
+#!/usr/bin/env node
 const electron = require('electron')
 const spawn = require('cross-spawn')
+const argv = process.argv.slice(2)
 
 const result = spawn.sync(
   electron,
-  [
-    require.resolve('./app')
-  ],
+  [require.resolve('./app')].concat(argv),
   { stdio: 'ignore' }
 )
 
