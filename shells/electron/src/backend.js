e3757d494d50f6cd09ed6772e8475d35c0846a34
Michał Sajnóg <msajnog93@gmail.com>
2018-01-15 19:14:25 +0100

    Handle disconnections, keep only one connection live

--- shells/electron/src/backend.js
+++ shells/electron/src/backend.js
@@ -1,6 +1,19 @@
 import io from 'socket.io-client'
 import { initBackend } from 'src/backend'
 import Bridge from 'src/bridge'
+import { installToast } from 'src/backend/toast'
+
+const connectedMessage = () => {
+  if (window.__VUE_DEVTOOLS_TOAST__) {
+    window.__VUE_DEVTOOLS_TOAST__('Remote Devtools Connected', 'normal')
+  }
+}
+
+const disconnectedMessage = () => {
+  if (window.__VUE_DEVTOOLS_TOAST__) {
+    window.__VUE_DEVTOOLS_TOAST__('Remote Devtools Disconnected', 'error')
+  }
+}
 
 (function () {
   const port = process.env.PORT || 8098
@@ -9,6 +22,10 @@ import Bridge from 'src/bridge'
   const bridge = new Bridge({
     listen (fn) {
       socket.on('vue-message', data => fn(data))
+      socket.on('vue-devtools-disconnect-backend', () => {
+        socket.disconnect()
+        disconnectedMessage()
+      })
     },
     send (data) {
       socket.emit('vue-message', data)
@@ -17,8 +34,11 @@ import Bridge from 'src/bridge'
 
   bridge.on('shutdown', () => {
     socket.disconnect()
+    disconnectedMessage()
   })
 
+  installToast(window)
   initBackend(bridge)
   socket.emit('vue-devtools-init')
+  connectedMessage()
 }())
