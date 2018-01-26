0d59351e1c629ed1026595fb3124a83307731f29
Guillaume Chau <alphadelta.fg@gmail.com>
2018-01-26 17:22:49 +0100

    Check for global hook var in all shells

--- shells/electron/src/hook.js
+++ shells/electron/src/hook.js
@@ -1,5 +1,3 @@
 import { installHook } from 'src/backend/hook'
 
-if (!window.__VUE_DEVTOOLS_GLOBAL_HOOK__) {
-  installHook(window)
-}
+installHook(window)
--- src/backend/hook.js
+++ src/backend/hook.js
@@ -12,75 +12,77 @@
 export function installHook (window) {
   let listeners = {}
 
-  const hook = {
-    Vue: null,
+  if (!window.hasOwnProperty('__VUE_DEVTOOLS_GLOBAL_HOOK__')) {
+    const hook = {
+      Vue: null,
 
-    on (event, fn) {
-      event = '$' + event
-      ;(listeners[event] || (listeners[event] = [])).push(fn)
-    },
+      on (event, fn) {
+        event = '$' + event
+        ;(listeners[event] || (listeners[event] = [])).push(fn)
+      },
 
-    once (event, fn) {
-      const eventAlias = event
-      event = '$' + event
-      function on () {
-        this.off(eventAlias, on)
-        fn.apply(this, arguments)
-      }
-      ;(listeners[event] || (listeners[event] = [])).push(on)
-    },
+      once (event, fn) {
+        const eventAlias = event
+        event = '$' + event
+        function on () {
+          this.off(eventAlias, on)
+          fn.apply(this, arguments)
+        }
+        ;(listeners[event] || (listeners[event] = [])).push(on)
+      },
 
-    off (event, fn) {
-      event = '$' + event
-      if (!arguments.length) {
-        listeners = {}
-      } else {
-        const cbs = listeners[event]
-        if (cbs) {
-          if (!fn) {
-            listeners[event] = null
-          } else {
-            for (let i = 0, l = cbs.length; i < l; i++) {
-              const cb = cbs[i]
-              if (cb === fn || cb.fn === fn) {
-                cbs.splice(i, 1)
-                break
+      off (event, fn) {
+        event = '$' + event
+        if (!arguments.length) {
+          listeners = {}
+        } else {
+          const cbs = listeners[event]
+          if (cbs) {
+            if (!fn) {
+              listeners[event] = null
+            } else {
+              for (let i = 0, l = cbs.length; i < l; i++) {
+                const cb = cbs[i]
+                if (cb === fn || cb.fn === fn) {
+                  cbs.splice(i, 1)
+                  break
+                }
               }
             }
           }
         }
-      }
-    },
+      },
 
-    emit (event) {
-      event = '$' + event
-      let cbs = listeners[event]
-      if (cbs) {
-        const args = [].slice.call(arguments, 1)
-        cbs = cbs.slice()
-        for (let i = 0, l = cbs.length; i < l; i++) {
-          cbs[i].apply(this, args)
+      emit (event) {
+        event = '$' + event
+        let cbs = listeners[event]
+        if (cbs) {
+          const args = [].slice.call(arguments, 1)
+          cbs = cbs.slice()
+          for (let i = 0, l = cbs.length; i < l; i++) {
+            cbs[i].apply(this, args)
+          }
         }
       }
     }
+
+    hook.once('init', Vue => {
+      hook.Vue = Vue
+
+      Vue.prototype.$inspect = function () {
+        const fn = window.__VUE_DEVTOOLS_INSPECT__
+        fn && fn(this)
+      }
+    })
+
+    hook.once('vuex:init', store => {
+      hook.store = store
+    })
+
+    Object.defineProperty(window, '__VUE_DEVTOOLS_GLOBAL_HOOK__', {
+      get () {
+        return hook
+      }
+    })
   }
-
-  hook.once('init', Vue => {
-    hook.Vue = Vue
-
-    Vue.prototype.$inspect = function () {
-      const fn = window.__VUE_DEVTOOLS_INSPECT__
-      fn && fn(this)
-    }
-  })
-
-  hook.once('vuex:init', store => {
-    hook.store = store
-  })
-
-  Object.defineProperty(window, '__VUE_DEVTOOLS_GLOBAL_HOOK__', {
-    get () {
-      return hook
-    }
-  })
 }
