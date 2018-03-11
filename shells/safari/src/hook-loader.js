2cabbbc25b8f29fc9426ab20f0b537cff04427c3
Guillaume Chau <alphadelta.fg@gmail.com>
2018-03-11 16:41:24 +0100

    End safari support

--- shells/safari/src/hook-loader.js
+++
@@ -1,5 +0,0 @@
-/*global safari*/
-const script = document.constructor.prototype.createElement.call(document, 'script')
-script.src = `${safari.extension.baseURI}build/hook.js`
-document.documentElement.appendChild(script)
-script.parentNode.removeChild(script)
