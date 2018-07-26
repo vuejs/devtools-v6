a5095c64d2c3170934383687464c25e05489fc15
Pooya Parsa <pyapar@gmail.com>
2018-07-26 22:09:50 +0430

    feat: detect Nuxt.js (#661)
    * feat: add chrome shell icons for nuxt.js
    
    * feat: add chrome popups for nuxt.js
    
    * feat: support icon/popup suffix
    
    * feat(chrome/detector): add nuxt detector

--- shells/chrome/src/detector.js
+++ shells/chrome/src/detector.js
@@ -9,6 +9,26 @@ window.addEventListener('message', e => {
 
 function detect (win) {
   setTimeout(() => {
+    // Method 1: Check Nuxt.js
+    const nuxtDetected = Boolean(window.__NUXT__ || window.$nuxt)
+
+    if (nuxtDetected) {
+      let Vue
+
+      if (window.$nuxt) {
+        Vue = window.$nuxt.$root.constructor
+      }
+
+      win.postMessage({
+        devtoolsEnabled: Vue && Vue.config.devtools,
+        vueDetected: true,
+        nuxtDetected: true
+      }, '*')
+
+      return
+    }
+
+    // Method 2: Scan all elements inside document
     const all = document.querySelectorAll('*')
     let el
     for (let i = 0; i < all.length; i++) {
