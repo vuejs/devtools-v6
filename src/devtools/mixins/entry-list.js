4cfac24137b82f1300d7703c451dbba1bdb56744
Guillaume Chau <alphadelta.fg@gmail.com>
2018-08-01 11:30:17 +0200

    fix: debounce scroll to selected item

--- src/devtools/mixins/entry-list.js
+++ src/devtools/mixins/entry-list.js
@@ -1,3 +1,5 @@
+import debounce from 'lodash.debounce'
+
 export default function ({
   indexOffset = 0
 } = {}) {
@@ -16,7 +18,7 @@ export default function ({
     },
 
     methods: {
-      scrollIntoInspected (index) {
+      scrollIntoInspected: debounce(function (index) {
         index += indexOffset
         this.$nextTick(() => {
           const scroller = this.$globalRefs.leftRecycleList || this.$globalRefs.leftScroll
@@ -30,7 +32,7 @@ export default function ({
             scroller.scrollTop = top + height - parentHeight
           }
         })
-      }
+      }, 30)
     }
   }
 }
