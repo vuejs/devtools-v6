9ba70282ccb77baaa9204090f71d0ed847004c80
Alex Kyriakidis <hootlex@icloud.com>
2018-08-02 13:20:40 +0200

    add vuex object elements to dev

---
+++ shells/dev/target/VuexObject.vue
@@ -0,0 +1,19 @@
+<template>
+  <div id="vuex-object">
+    <h2>Vuex Object</h2>
+    <pre><code>{{ object }}</code></pre>
+  </div>
+</template>
+
+<script>
+  import {mapState} from 'vuex'
+  export default {
+    computed: {
+      ...mapState(['object'])
+    },
+  }
+</script>
+
+<style scoped>
+
+</style>
