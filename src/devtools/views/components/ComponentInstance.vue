5b36bf6cddab2dbe2a43e2246571867738cfa5df
1099511627776 <1099511627776@mail.ru>
2019-01-27 12:42:50 +0200

    fix lint errors

--- src/devtools/views/components/ComponentInstance.vue
+++ src/devtools/views/components/ComponentInstance.vue
@@ -45,11 +45,17 @@
           <span class="attr-title"> key</span>=<span class="attr-value">{{ instance.renderKey }}</span>
         </span>
 
-        <span v-if="componentHasNameProp" class="attr">
+        <span
+          v-if="componentHasNameProp"
+          class="attr"
+        >
           <span class="attr-title"> name</span>=<span class="attr-value">{{ instance.nameProp }}</span>
         </span>
 
-        <span v-if="componentHasTagProp" class="attr">
+        <span
+          v-if="componentHasTagProp"
+          class="attr"
+        >
           <span class="attr-title"> tag</span>=<span class="attr-value">{{ instance.tagProp }}</span>
         </span>
 
@@ -157,11 +163,11 @@ export default {
       return (this.instance.renderKey === 0 || !!this.instance.renderKey) && this.instance.renderKey !== UNDEFINED
     },
 
-    componentHasNameProp(){
+    componentHasNameProp () {
       return (this.instance.nameProp === 0 || !!this.instance.nameProp) && this.instance.nameProp !== UNDEFINED
     },
 
-    componentHasTagProp(){
+    componentHasTagProp () {
       return (this.instance.tagProp === 0 || !!this.instance.tagProp) && this.instance.tagProp !== UNDEFINED
     }
   },
