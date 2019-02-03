cf74ad3ce66534ea028e953ca4ccf85835114f7f
Guillaume Chau <alphadelta.fg@gmail.com>
2019-02-03 19:31:51 +0100

    fix: update clone code to fix 'illegal invocation' error

--- src/backend/hook.js
+++ src/backend/hook.js
@@ -108,12 +108,36 @@ export function installHook (target) {
   })
 
   // Clone deep utility for cloning initial state of the store
-  // REFERENCE: https://github.com/buunguyen/node-clone/commit/63afda9de9d94b9332586e34a646a13e8d719244
+  // REFERENCE: https://github.com/pvorb/clone
 
-  function clone (parent, circular, depth, prototype) {
+  let NativeMap
+  try {
+    NativeMap = Map
+  } catch (_) {
+    // maybe a reference error because no `Map`. Give it a dummy value that no
+    // value will ever be an instanceof.
+    NativeMap = function () {}
+  }
+
+  let NativeSet
+  try {
+    NativeSet = Set
+  } catch (_) {
+    NativeSet = function () {}
+  }
+
+  let NativePromise
+  try {
+    NativePromise = Promise
+  } catch (_) {
+    NativePromise = function () {}
+  }
+
+  function clone (parent, circular, depth, prototype, includeNonEnumerable) {
     if (typeof circular === 'object') {
       depth = circular.depth
       prototype = circular.prototype
+      includeNonEnumerable = circular.includeNonEnumerable
       circular = circular.circular
     }
     // maintain two arrays for circular references, where corresponding parents
@@ -140,30 +164,37 @@ export function installHook (target) {
         return parent
       }
 
-      if (parent instanceof Map) {
-        child = new Map()
-      } else if (parent instanceof Set) {
-        child = new Set()
-      } else if (parent instanceof Promise) {
-        child = new Promise(function (resolve, reject) {
+      if (_instanceof(parent, NativeMap)) {
+        child = new NativeMap()
+      } else if (_instanceof(parent, NativeSet)) {
+        child = new NativeSet()
+      } else if (_instanceof(parent, NativePromise)) {
+        child = new NativePromise(function (resolve, reject) {
           parent.then(function (value) {
             resolve(_clone(value, depth - 1))
           }, function (err) {
             reject(_clone(err, depth - 1))
           })
         })
-      } else if (_isArray(parent)) {
+      } else if (clone.__isArray(parent)) {
         child = []
-      } else if (_isRegExp(parent)) {
-        child = new RegExp(parent.source, _getRegExpFlags(parent))
+      } else if (clone.__isRegExp(parent)) {
+        child = new RegExp(parent.source, __getRegExpFlags(parent))
         if (parent.lastIndex) child.lastIndex = parent.lastIndex
-      } else if (_isDate(parent)) {
+      } else if (clone.__isDate(parent)) {
         child = new Date(parent.getTime())
       } else if (useBuffer && Buffer.isBuffer(parent)) {
-        child = Buffer.alloc(parent.length)
-        parent.copy(child)
+        if (Buffer.from) {
+          // Node.js >= 5.10.0
+          child = Buffer.from(parent)
+        } else {
+          // Older Node.js versions
+          // eslint-disable-next-line node/no-deprecated-api
+          child = new Buffer(parent.length)
+          parent.copy(child)
+        }
         return child
-      } else if (parent instanceof Error) {
+      } else if (_instanceof(parent, Error)) {
         child = Object.create(parent)
       } else {
         if (typeof prototype === 'undefined') {
@@ -185,40 +216,25 @@ export function installHook (target) {
         allChildren.push(child)
       }
 
-      if (parent instanceof Map) {
-        var keyIterator = parent.keys()
-        while (true) {
-          let next = keyIterator.next()
-          if (next.done) {
-            break
-          }
-          var keyChild = _clone(next.value, depth - 1)
-          var valueChild = _clone(parent.get(next.value), depth - 1)
+      if (_instanceof(parent, NativeMap)) {
+        parent.forEach(function (value, key) {
+          var keyChild = _clone(key, depth - 1)
+          var valueChild = _clone(value, depth - 1)
           child.set(keyChild, valueChild)
-        }
+        })
       }
-      if (parent instanceof Set) {
-        var iterator = parent.keys()
-        while (true) {
-          let next = iterator.next()
-          if (next.done) {
-            break
-          }
-          var entryChild = _clone(next.value, depth - 1)
+      if (_instanceof(parent, NativeSet)) {
+        parent.forEach(function (value) {
+          var entryChild = _clone(value, depth - 1)
           child.add(entryChild)
-        }
+        })
       }
 
-      for (let i in parent) {
-        var attrs
-        if (proto) {
-          attrs = Object.getOwnPropertyDescriptor(proto, i)
+      for (var i in parent) {
+        var attrs = Object.getOwnPropertyDescriptor(parent, i)
+        if (attrs) {
+          child[i] = _clone(parent[i], depth - 1)
         }
-
-        if (attrs && attrs.set == null) {
-          continue
-        }
-        child[i] = _clone(parent[i], depth - 1)
       }
 
       if (Object.getOwnPropertySymbols) {
@@ -228,10 +244,24 @@ export function installHook (target) {
           // like a number or string.
           var symbol = symbols[i]
           var descriptor = Object.getOwnPropertyDescriptor(parent, symbol)
-          if (descriptor && !descriptor.enumerable) {
+          if (descriptor && !descriptor.enumerable && !includeNonEnumerable) {
             continue
           }
           child[symbol] = _clone(parent[symbol], depth - 1)
+          Object.defineProperty(child, symbol, descriptor)
+        }
+      }
+
+      if (includeNonEnumerable) {
+        var allPropertyNames = Object.getOwnPropertyNames(parent)
+        for (let i = 0; i < allPropertyNames.length; i++) {
+          const propertyName = allPropertyNames[i]
+          let descriptor = Object.getOwnPropertyDescriptor(parent, propertyName)
+          if (descriptor && descriptor.enumerable) {
+            continue
+          }
+          child[propertyName] = _clone(parent[propertyName], depth - 1)
+          Object.defineProperty(child, propertyName, descriptor)
         }
       }
 
@@ -243,27 +273,36 @@ export function installHook (target) {
 
   // private utility functions
 
-  function _objToStr (o) {
+  function __objToStr (o) {
     return Object.prototype.toString.call(o)
   }
+  clone.__objToStr = __objToStr
 
-  function _isDate (o) {
-    return typeof o === 'object' && _objToStr(o) === '[object Date]'
+  function __isDate (o) {
+    return typeof o === 'object' && __objToStr(o) === '[object Date]'
   }
+  clone.__isDate = __isDate
 
-  function _isArray (o) {
-    return typeof o === 'object' && _objToStr(o) === '[object Array]'
+  function __isArray (o) {
+    return typeof o === 'object' && __objToStr(o) === '[object Array]'
   }
+  clone.__isArray = __isArray
 
-  function _isRegExp (o) {
-    return typeof o === 'object' && _objToStr(o) === '[object RegExp]'
+  function __isRegExp (o) {
+    return typeof o === 'object' && __objToStr(o) === '[object RegExp]'
   }
+  clone.__isRegExp = __isRegExp
 
-  function _getRegExpFlags (re) {
+  function __getRegExpFlags (re) {
     var flags = ''
     if (re.global) flags += 'g'
     if (re.ignoreCase) flags += 'i'
     if (re.multiline) flags += 'm'
     return flags
+  }
+  clone.__getRegExpFlags = __getRegExpFlags
+
+  function _instanceof (obj, type) {
+    return type != null && obj instanceof type
   }
 }
