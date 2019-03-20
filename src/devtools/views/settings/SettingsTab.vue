711f2e0dfed579f681f6cf03f0c8cf5f8ea551c8
Guillaume Chau <alphadelta.fg@gmail.com>
2019-03-20 17:03:10 +0100

    fix(settings): style tweaks

--- src/devtools/views/settings/SettingsTab.vue
+++ src/devtools/views/settings/SettingsTab.vue
@@ -22,9 +22,14 @@ export default {
   >>> .preferences
     display flex
     flex-wrap wrap
-    padding 1rem 1.5rem
+    padding 12px 4px
 
     > *
       flex-basis 300px
-      margin 10px 18px
+      margin 16px 24px
+
+    .vue-ui-form-field
+      > .wrapper > .content
+       min-height 32px
+       justify-content center
 </style>
