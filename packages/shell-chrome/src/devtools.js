66a6493d42ca5b13e4c90e30a92ed01b832dd7f9
Guillaume Chau <alphadelta.fg@gmail.com>
2019-10-28 13:43:19 +0100

    chore: debug bridge in dev chrome extension

--- packages/shell-chrome/src/devtools.js
+++ packages/shell-chrome/src/devtools.js
@@ -29,6 +29,9 @@ initDevTools({
         },
         send (data) {
           if (!disconnected) {
+            if (process.env.NODE_ENV !== 'production') {
+              console.log('devtools -> backend', data)
+            }
             port.postMessage(data)
           }
         }
