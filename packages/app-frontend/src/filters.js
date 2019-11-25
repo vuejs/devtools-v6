7fd8a5a0ad568b9ee0b52c61489ae59912327718
Guillaume Chau <alphadelta.fg@gmail.com>
2019-11-25 17:40:31 +0100

    fix: pad milliseconds, closes #1109

--- packages/app-frontend/src/filters.js
+++ packages/app-frontend/src/filters.js
@@ -13,7 +13,7 @@ const specialTypeRE = /^\[native (\w+) (.*)\]$/
 
 export function formatTime (timestamp, format) {
   const date = new Date(timestamp)
-  return `${date.toString().match(/\d\d:\d\d:\d\d/)[0]}${format === 'ms' ? '.' + date.getMilliseconds() : ''}`
+  return `${date.toString().match(/\d\d:\d\d:\d\d/)[0]}${format === 'ms' ? '.' + String(date.getMilliseconds()).padStart(3, '0') : ''}`
 }
 
 export function valueType (value) {
