3f92f467d00b3395d3f0e721e32b6303b928c682
Eduardo San Martin Morote <posva13@gmail.com>
2020-10-07 14:36:07 +0200

    style: remove extra formatting

--- packages/api/src/index.ts
+++ packages/api/src/index.ts
@@ -12,17 +12,13 @@ export interface PluginDescriptor {
 
 export type SetupFunction = (api: DevtoolsPluginApi) => void
 
-export function setupDevtoolsPlugin (
-  pluginDescriptor: PluginDescriptor,
-  setupFn: SetupFunction
-) {
+export function setupDevtoolsPlugin (pluginDescriptor: PluginDescriptor, setupFn: SetupFunction) {
   const hook = getDevtoolsGlobalHook()
   if (hook) {
     hook.emit(HOOK_SETUP, pluginDescriptor, setupFn)
   } else {
     const target = getTarget()
-    const list = (target.__VUE_DEVTOOLS_PLUGINS__ =
-      target.__VUE_DEVTOOLS_PLUGINS__ || [])
+    const list = target.__VUE_DEVTOOLS_PLUGINS__ = target.__VUE_DEVTOOLS_PLUGINS__ || []
     list.push({
       pluginDescriptor,
       setupFn
