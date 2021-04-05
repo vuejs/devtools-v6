2312a3aceada22de09dfa1428e61ffda93404cf9
Joshua Sosso <josh@modiimedia.com>
2021-04-05 17:45:06 -0500

    add missing data to "shell-electron/types"

--- packages/shell-electron/types/index.d.ts
+++ packages/shell-electron/types/index.d.ts
@@ -1,1 +1,15 @@
-export function connect(host?: string, port?: number|string): void
+import type Vue from "vue";
+
+interface ConnectOptions {
+  io?: Function;
+  showToast?: Function;
+  app?: Vue | Vue[];
+}
+
+export function connect(
+  host?: string,
+  port?: number | string,
+  options: ConnectOptions
+): void;
+
+export function init(vue: Vue | Vue[]): void;
