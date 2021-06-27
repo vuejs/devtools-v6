7ce7ba5dbf340352f6d9e74b68e88c8806aebe02
Guillaume <guillaume.b.chau@gmail.com>
2021-06-28 01:07:41 +0200

    docs: favicon + header logo

--- docs/.vitepress/config.js
+++ docs/.vitepress/config.js
@@ -2,8 +2,14 @@ module.exports = {
   title: 'Vue Devtools',
   description: 'Browser devtools extension for debugging Vue.js applications',
 
+  head: [
+    ['link', { rel: 'icon', type: 'image/svg+xml', href: '/favicon.svg' }],
+    ['link', { rel: 'icon', type: 'image/png', href: '/favicon.png' }],
+  ],
+
   themeConfig: {
     repo: 'vuejs/vue-devtools',
+    logo: '/logo-header.svg',
     docsDir: 'docs',
     docsBranch: 'next',
     editLinks: true,
