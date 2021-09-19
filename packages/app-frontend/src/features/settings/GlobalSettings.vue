c283e85a8a3ce345ffe8a8b0a1c113ed5a7830ca
Guillaume Chau <guillaume.b.chau@gmail.com>
2021-09-19 09:18:32 +0200

    fix(settings): items height

--- packages/app-frontend/src/features/settings/GlobalSettings.vue
+++ packages/app-frontend/src/features/settings/GlobalSettings.vue
@@ -13,59 +13,61 @@ export default defineComponent({})
 </script>
 
 <template>
-  <div class="global-preferences preferences">
-    <VueFormField title="Theme">
-      <VueGroup
-        v-model="$shared.theme"
-        class="extend w-96"
+  <div class="global-preferences">
+    <div class="preferences">
+      <VueFormField title="Theme">
+        <VueGroup
+          v-model="$shared.theme"
+          class="extend w-96"
+        >
+          <VueGroupButton
+            value="auto"
+            label="Auto"
+          />
+          <VueGroupButton
+            value="light"
+            label="Light"
+          />
+          <VueGroupButton
+            value="dark"
+            label="Dark"
+          />
+          <VueGroupButton
+            value="high-contrast"
+            label="High contrast"
+          />
+        </VueGroup>
+      </VueFormField>
+
+      <VueFormField
+        title="Menu Step Scrolling"
       >
-        <VueGroupButton
-          value="auto"
-          label="Auto"
-        />
-        <VueGroupButton
-          value="light"
-          label="Light"
-        />
-        <VueGroupButton
-          value="dark"
-          label="Dark"
-        />
-        <VueGroupButton
-          value="high-contrast"
-          label="High contrast"
-        />
-      </VueGroup>
-    </VueFormField>
+        <VueSwitch v-model="$shared.menuStepScrolling">
+          Enable
+        </VueSwitch>
+        <template #subtitle>
+          Useful for trackpads
+        </template>
+      </VueFormField>
 
-    <VueFormField
-      title="Menu Step Scrolling"
-    >
-      <VueSwitch v-model="$shared.menuStepScrolling">
-        Enable
-      </VueSwitch>
-      <template #subtitle>
-        Useful for trackpads
-      </template>
-    </VueFormField>
+      <VueFormField
+        title="Performance monitoring"
+      >
+        <VueSwitch v-model="$shared.performanceMonitoringEnabled">
+          Enable
+        </VueSwitch>
+        <template #subtitle>
+          Turn off if your app is slowed down
+        </template>
+      </VueFormField>
 
-    <VueFormField
-      title="Performance monitoring"
-    >
-      <VueSwitch v-model="$shared.performanceMonitoringEnabled">
-        Enable
-      </VueSwitch>
-      <template #subtitle>
-        Turn off if your app is slowed down
-      </template>
-    </VueFormField>
-
-    <VueFormField
-      title="Debugging info"
-    >
-      <VueSwitch v-model="$shared.debugInfo">
-        Enable
-      </VueSwitch>
-    </VueFormField>
+      <VueFormField
+        title="Debugging info"
+      >
+        <VueSwitch v-model="$shared.debugInfo">
+          Enable
+        </VueSwitch>
+      </VueFormField>
+    </div>
   </div>
 </template>
