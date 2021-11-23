b71ae4ad719b11717dbfa3af4493ebfaa3c0cc24
Josh Michaels <31548851+joshmossas@users.noreply.github.com>
2021-11-23 12:07:40 -0600

    fix(types): Add Missing Methods & Props to "./packages/shell-electron/types" (#1397)

--- packages/shell-electron/types/index.d.ts
+++ packages/shell-electron/types/index.d.ts
@@ -1,1 +1,15 @@
-export function connect(host?: string, port?: number|string): void
+import type { VueConstructor } from "vue";
+
+interface ConnectOptions {
+  io?: Function;
+  showToast?: Function;
+  app?: VueConstructor | VueConstructor[];
+}
+
+export function connect(
+  host?: string,
+  port?: number | string,
+  options?: ConnectOptions
+): void;
+
+export function init(vue: VueConstructor | VueConstructor[]): void;
