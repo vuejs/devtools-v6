3d0775387ab36afe4c60d66c30f75518871d5b61
jingyu <jingyu@gaoding.com>
2022-06-16 16:23:34 +0800

    feat: feat: make components mousein hight event throttle

--- packages/app-frontend/src/features/components/composable/highlight.ts
+++ packages/app-frontend/src/features/components/composable/highlight.ts
@@ -2,15 +2,47 @@ import { useBridge } from '@front/features/bridge'
 import { BridgeEvents } from '@vue-devtools/shared-utils'
 import { Ref } from '@vue/composition-api'
 
+function handleThrottle (duration = 200) {
+  let timer: ReturnType<typeof setTimeout> | undefined
+  let isExecuting = false
+
+  const clear = () => {
+    if (timer) {
+      clearTimeout(timer)
+      timer = undefined
+    }
+  }
+
+  function exec (fn) {
+    if (isExecuting) {
+      clear()
+      timer = setTimeout(() => {
+        isExecuting = true
+        fn()
+      }, duration)
+    } else {
+      timer = setTimeout(() => {
+        isExecuting = false
+      }, duration)
+      fn()
+    }
+    isExecuting = true
+  }
+
+  return exec
+}
+
+const execThrottleFn = handleThrottle()
+
 export function useComponentHighlight (id: Ref<string>) {
   const { bridge } = useBridge()
 
   function highlight () {
-    bridge.send(BridgeEvents.TO_BACK_COMPONENT_MOUSE_OVER, id.value)
+    execThrottleFn(() => bridge.send(BridgeEvents.TO_BACK_COMPONENT_MOUSE_OVER, id.value))
   }
 
   function unhighlight () {
-    bridge.send(BridgeEvents.TO_BACK_COMPONENT_MOUSE_OUT)
+    execThrottleFn(() => bridge.send(BridgeEvents.TO_BACK_COMPONENT_MOUSE_OUT))
   }
 
   return {
