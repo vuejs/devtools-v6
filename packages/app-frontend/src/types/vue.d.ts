e8ea3c86080555a85d8a1e924502aff8dda0e26e
Fan Pei <fanpei920@gmail.com>
2023-10-19 00:38:04 +0900

    feat: Add global variable definition

---
+++ packages/app-frontend/src/types/vue.d.ts
@@ -0,0 +1,22 @@
+import {
+  SharedData,
+  keys,
+} from '@vue-devtools/shared-utils'
+
+import type {
+  Responsive,
+} from '@front/plugins/responsive'
+
+declare module '@vue/runtime-core' {
+  interface ComponentCustomProperties {
+    $t: (string, values?: Record<string, any>) => string
+    $responsive: Responsive
+    $shared: typeof SharedData
+    $isChrome: boolean
+    $isFirefox: boolean
+    $isWindows: boolean
+    $isMac: boolean
+    $isLinux: boolean
+    $keys: typeof keys
+  }
+}
