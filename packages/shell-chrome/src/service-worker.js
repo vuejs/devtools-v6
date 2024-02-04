38684af4e9039ff615a4edbc1d3fc75a074c8766
Guillaume Chau <guillaume.b.chau@gmail.com>
2024-02-04 15:31:44 +0100

    refactor: migrate to manifest v3
    based on #2136

--- packages/shell-chrome/src/service-worker.js
+++ packages/shell-chrome/src/service-worker.js
@@ -18,7 +18,7 @@ chrome.runtime.onConnect.addListener(port => {
   if (!ports[tab]) {
     ports[tab] = {
       devtools: null,
-      backend: null
+      backend: null,
     }
   }
   ports[tab][name] = port
@@ -33,13 +33,17 @@ function isNumeric (str) {
 }
 
 function installProxy (tabId) {
-  chrome.tabs.executeScript(tabId, {
-    file: '/build/proxy.js'
+  chrome.scripting.executeScript({
+    target: { tabId },
+    files: ['build/proxy.js'],
   }, function (res) {
     if (!res) {
       ports[tabId].devtools.postMessage('proxy-fail')
     } else {
-      console.log('injected proxy to tab ' + tabId)
+      if (process.env.NODE_ENV !== 'production') {
+        // eslint-disable-next-line no-console
+        console.log('injected proxy to tab ' + tabId)
+      }
     }
   })
 }
@@ -48,76 +52,77 @@ function doublePipe (id, one, two) {
   one.onMessage.addListener(lOne)
   function lOne (message) {
     if (message.event === 'log') {
+      // eslint-disable-next-line no-console
       return console.log('tab ' + id, message.payload)
     }
-    console.log('devtools -> backend', message)
+    if (process.env.NODE_ENV !== 'production') {
+      // eslint-disable-next-line no-console
+      console.log('%cdevtools -> backend', 'color:#888;', message)
+    }
     two.postMessage(message)
   }
   two.onMessage.addListener(lTwo)
   function lTwo (message) {
     if (message.event === 'log') {
+      // eslint-disable-next-line no-console
       return console.log('tab ' + id, message.payload)
     }
-    console.log('backend -> devtools', message)
+    if (process.env.NODE_ENV !== 'production') {
+      // eslint-disable-next-line no-console
+      console.log('%cbackend -> devtools', 'color:#888;', message)
+    }
     one.postMessage(message)
   }
   function shutdown () {
-    console.log('tab ' + id + ' disconnected.')
+    if (process.env.NODE_ENV !== 'production') {
+      // eslint-disable-next-line no-console
+      console.log('tab ' + id + ' disconnected.')
+    }
     one.onMessage.removeListener(lOne)
     two.onMessage.removeListener(lTwo)
     one.disconnect()
     two.disconnect()
     ports[id] = null
-    updateContextMenuItem()
   }
   one.onDisconnect.addListener(shutdown)
   two.onDisconnect.addListener(shutdown)
-  console.log('tab ' + id + ' connected.')
-  updateContextMenuItem()
+  if (process.env.NODE_ENV !== 'production') {
+    // eslint-disable-next-line no-console
+    console.log('tab ' + id + ' connected.')
+  }
 }
 
 chrome.runtime.onMessage.addListener((req, sender) => {
   if (sender.tab && req.vueDetected) {
     const suffix = req.nuxtDetected ? '.nuxt' : ''
 
-    chrome.browserAction.setIcon({
+    chrome.action.setIcon({
       tabId: sender.tab.id,
       path: {
-        16: `icons/16${suffix}.png`,
-        48: `icons/48${suffix}.png`,
-        128: `icons/128${suffix}.png`
-      }
+        16: chrome.runtime.getURL(`icons/16${suffix}.png`),
+        48: chrome.runtime.getURL(`icons/48${suffix}.png`),
+        128: chrome.runtime.getURL(`icons/128${suffix}.png`),
+      },
+    }, () => {
+      // noop
     })
-    chrome.browserAction.setPopup({
+    chrome.action.setPopup({
       tabId: sender.tab.id,
-      popup: req.devtoolsEnabled ? `popups/enabled${suffix}.html` : `popups/disabled${suffix}.html`
+      popup: chrome.runtime.getURL(req.devtoolsEnabled ? `popups/enabled${suffix}.html` : `popups/disabled${suffix}.html`),
+    }, () => {
+      // noop
     })
   }
-})
 
-// Right-click inspect context menu entry
-let activeTabId
-chrome.tabs.onActivated.addListener(({ tabId }) => {
-  activeTabId = tabId
-  updateContextMenuItem()
-})
-
-function updateContextMenuItem () {
-  chrome.contextMenus.removeAll(() => {
-    if (ports[activeTabId]) {
-      chrome.contextMenus.create({
-        id: 'vue-inspect-instance',
-        title: 'Inspect Vue component',
-        contexts: ['all']
+  if (req.action === 'vue-take-screenshot' && sender.envType === 'devtools_child') {
+    browser.tabs.captureVisibleTab({
+      format: 'png',
+    }).then(dataUrl => {
+      browser.runtime.sendMessage({
+        action: 'vue-screenshot-result',
+        id: req.id,
+        dataUrl,
       })
-    }
-  })
-}
-
-chrome.contextMenus.onClicked.addListener((info, tab) => {
-  chrome.runtime.sendMessage({
-    vueContextMenu: {
-      id: info.menuItemId
-    }
-  })
+    })
+  }
 })
